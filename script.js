912d212a062b6a6a22abcc1dfa18b97120b1a019
Lovisa Elensky <lovisa@MacBook-Pro.local>
2020-10-01 08:13:09 +0200

    Second version

--- script.js
+++ script.js
@@ -1,27 +1,61 @@
-var addToDoButton = document.getElementById('add-btn');
-var toDoContainer = document.getElementById('toDoContainer');
-var inputField = document.getElementById('inputField');
 
-addToDoButton.addEventListener('click', function(){
-    var paragraph = document.createElement('p');
-    paragraph.innerText = inputField.value;
-    toDoContainer.appendChild(paragraph);
-    inputField.value = "";
+var button = document.getElementById("add-btn");
+var newItem = document.getElementById("userInput");
+var ul = document.querySelector("ul");
+var list = document.getElementsByTagName("li");
+var del = document.getElementsByClassName("delete-btn");
 
-    paragraph.addEventListener('click', function(){
-        paragraph.style.textDecoration = "line-through";
-    })
 
-});
+for (var i = 0; i < del.length; i++) {
+  del[i].addEventListener("click", deleteListElement);
+}
 
-inputField.addEventListener('keypress', function(e){
-    if  (e.code === 'Enter') {
-        var paragraph = document.createElement('p');
-        paragraph.innerText = inputField.value;
-        toDoContainer.appendChild(paragraph);
-        inputField.value = "";
-    }
-    paragraph.addEventListener('click', function(){
-        paragraph.style.textDecoration = "line-through";
-    })
-});
+for (var i = 0; i < list.length; i++) {
+  list[i].addEventListener("click", liClick);
+}
+
+// removes parent element (since button is a child)
+function deleteListElement() {
+  this.parentElement.remove();
+}
+
+function liClick() {
+  this.classList.toggle("done");
+}
+
+function inputLength() {
+  return newItem.value.length;
+}
+
+function createListElement() {
+  var li = document.createElement("li");
+  li.innerText = newItem.value;
+  ul.appendChild(li);
+  newItem.value = "";
+
+  var deleteBtn = document.createElement("button");
+  deleteBtn.appendChild(document.createTextNode("Delete"));
+  li.appendChild(deleteBtn);
+  ul.appendChild(li);
+
+  // Register action on newly added elements
+  li.addEventListener("click", liClick);
+  deleteBtn.addEventListener("click", deleteListElement);
+}
+
+function addListAfterClick() {
+  if (inputLength() > 0) {
+    createListElement();
+  } else {
+    alert("Write something!");
+  }
+}
+
+function addListAfterEnter(event) {
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
+}
+
+button.addEventListener("click", addListAfterClick);
+newItem.addEventListener("keypress", addListAfterEnter);
